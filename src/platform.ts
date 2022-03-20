e7bc131c1cb56c2ff53eb2642f853260a0d48367
Mikhail Beliakov <miklebel97@gmail.com>
2022-03-21 00:59:33 +0300

    uuid fix

--- src/platform.ts
+++ src/platform.ts
@@ -75,7 +75,7 @@ export class SteamlinxHomebridgeHomebridgePlugin implements DynamicPlatformPlugi
       // generate a unique id for the accessory this should be generated from
       // something globally unique, but constant, for example, the device serial
       // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.VMAccessToken);
+      const uuid = this.api.hap.uuid.generate(`${device.DeviceName}${device.VMAccessToken}`);
 
       // see if an accessory with the same uuid has already been registered and restored from
       // the cached devices we stored in the `configureAccessory` method above
